b3b94c5be51e427f4126892cfa3075003f0725db
JesperK-EAA <eaa26jekj@students.eaaa.dk>
2026-05-26 09:55:22 +0200

    feat: faq card can open and lose now with a few changes in html and css for  small improvements

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-'use strict'
-/* fast test  */
+"use strict";
+
+/* Faq open and losing of faq cards */
+
+function faqDropdown(event) {
+  let arrowIcon = event.querySelector("img");
+
+  const faqCard = event.querySelector(".faq-question-info");
+  faqCard.classList.toggle("open");
+
+  if (arrowIcon.src.includes("open-arrow.svg")) {
+    arrowIcon.src = "img/icons/closed-arrow.svg";
+  } else {
+    arrowIcon.src = "img/icons/open-arrow.svg";
+  }
+}
